3ab8d4f361127112ad61fe30aefe9ad13fce6e96
蒲立望60263 <liwang.pu@going-link.com>
2023-07-03 14:52:31 +0800

    less学习项目添加dev serve 为了测试更加方便

--- packages/less-learn/webpack.config.js
+++ packages/less-learn/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -15,7 +17,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '../../dist/less-learn'),
-    clean: false,
+    clean: true,
     // comments: false,
   },
   module: {
@@ -30,8 +32,10 @@ module.exports = {
               sourceMap: false,
               modules: {
                 // localIdentName: "[local]--[hash:base64:5]",
-                // 学习项目为了直观一些,就不加hash
-                localIdentName: "[local]",
+                // 学习项目为了直观一些,就不加hash,但是不加又分不出:gloabl()效果,所以可以直接写出固定值
+                localIdentName: "[local]--HASH",
+                // 下面这种是不加hash的
+                // localIdentName: "[local]",
               },
             }
           },
@@ -40,5 +44,22 @@ module.exports = {
       }
     ]
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: '代码分隔',
+      template: './public/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    // compress: true,
+    port: 9000,
+  },
 };
